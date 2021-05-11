70c2fb10d9d9bc3614fceff7f983fe312cf0cb2f
andjelajankovic <76687161+andjelajankovic@users.noreply.github.com>
2021-05-11 10:27:20 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,35 @@
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <h2>Контакт</h2>
+            <div class="row justify-content-between">
+                <div class="col-md-3">
+                    <img src="img/logo.jpg" alt="" class="img-fluid">
+                </div>
+                <div class="col-md-4">
+                    <p><i class="fas fa-envelope"></i>&nbsp;radoststvaranjans@gmail.com</p>
+                    <p><i class="fas fa-phone"></i>&nbsp;065 2016300</p>
+                    <p><a href=""><i class="fab fa-facebook"></i>&nbsp;ФБ страница</a></p>
+                </div>
+                <div class="col-md-4">
+                    <h4>O nama</h4>
+                    <p>Нестраначко, невладино и непрофитно удружење за децу и омладину- кроз игру, машту и стваралаштво, стварамо бољи свет!</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- bootom bar -->
+    <section class="bottomBar text-center">
+        <p>Израда сајта <a href="https://webcon.rs">webcon.rs</a></p>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="js/typed.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.filterizr.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
